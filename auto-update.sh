7259e8ac78271f121986b929a4ace1e5768b6cac
James <jmz.taylor16@gmail.com>
2021-06-13 18:29:14 -0500

    Fallback to old ssh-keygen commands

--- auto-update.sh
+++ auto-update.sh
@@ -8,7 +8,13 @@ if [[ $# -eq 2 ]] ; then
     sed -i 's/PermitRootLogin.*/PermitRootLogin no/g' /etc/ssh/sshd_config
     sed -i '/PasswordAuthentication/c\#PasswordAuthentication no' /etc/ssh/sshd_config
     ssh-keygen -M generate -O bits=2048 moduli-2048.candidates
+    if [ $? -ne 0 ]; then
+        ssh-keygen -G moduli-2048.candidates -b 2048
+    fi
     ssh-keygen -M screen -f moduli-2048.candidates moduli-2048
+    if [ $? -ne 0 ]; then
+        ssh-keygen -T moduli-2048 -f moduli-2048.candidates
+    fi
     mv moduli-2048 /etc/ssh/moduli
     echo 'KexAlgorithms curve25519-sha256@libssh.org' >> /etc/ssh/sshd_config
     echo 'Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr' >> /etc/ssh/sshd_config
@@ -16,7 +22,7 @@ if [[ $# -eq 2 ]] ; then
     sed -i 's/^\#HostKey \/etc\/ssh\/ssh_host_\(rsa\|ed25519\)_key$/HostKey \/etc\/ssh\/ssh_host_\1_key/g' /etc/ssh/sshd_config
     sed -i 's/X11Forwarding.*/X11Forwarding no/g' /etc/ssh/sshd_config
     sed -i 's/PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ssh/sshd_config
-    echo -e "\n# Restrict key exchange, cipher, and MAC algorithms, as per sshaudit.com\n# hardening guide.\nKexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256\nCiphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr\nMACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com\nHostKeyAlgorithms ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,sk-ssh-ed25519@openssh.com,sk-ssh-ed25519-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-512,rsa-sha2-256-cert-v01@openssh.com,rsa-sha2-512-cert-v01@openssh.com" > /etc/ssh/sshd_config.d/ssh-audit_hardening.conf
+    echo -e "\n# Restrict key exchange, cipher, and MAC algorithms, as per sshaudit.com\n# hardening guide.\nKexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256\nCiphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr\nMACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com\nHostKeyAlgorithms ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,sk-ssh-ed25519@openssh.com,sk-ssh-ed25519-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-512,rsa-sha2-256-cert-v01@openssh.com,rsa-sha2-512-cert-v01@openssh.com" >> /etc/ssh/sshd_config.conf
     rm /etc/ssh/ssh_host_*
     ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N ""
     ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ""
